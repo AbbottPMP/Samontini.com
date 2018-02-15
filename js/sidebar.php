ae1dbb5aa58b94ca6c062920486fefd42d337af1
Sam Abbott <AbbottPMP@gmail.com>
2018-02-15 16:08:45 -0500

    Create sidebar.php

---
+++ js/sidebar.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+
+* @package wordpress
+
+* @subpackage sam
+
+*/
+
+
+?>
+
+<div class="col-xs-12 col-sm-4 sidebar_main">
+  <div class="row">
+    <!-- <?php if ( !function_exists( 'dynamic_sidebar' ) || !dynamic_sidebar('page_sidebar') ) ?> -->
+  </div>
+</div>
